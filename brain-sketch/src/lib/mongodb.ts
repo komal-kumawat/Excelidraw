edcd392e410de5293077e0d41b37cec6e5c3c3a7
komal-kumawat <kumawatkomal17122004@gmail.com>
2026-01-21 10:20:46 +0530

    mongo done

--- brain-sketch/src/lib/mongodb.ts
+++ brain-sketch/src/lib/mongodb.ts
@@ -1,50 +1,53 @@
 import mongoose from "mongoose";
 
-const mongodb_url = process.env.MONGO_URI as string ;
+const mongodb_url = process.env.MONGO_URI as string;
 
-if(!mongodb_url){
-    throw new Error("mongodb url not found please define it in .env file");
-
+if (!mongodb_url) {
+  throw new Error("MongoDB URI not found. Please define it in .env file");
 }
 
 interface GlobalMongoose {
-    conn:typeof mongoose|null;
-    promise:Promise<typeof mongoose>|null;
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
 }
 
-declare global{
-    var mongoose:GlobalMongoose
+declare global {
+  var mongoose: GlobalMongoose;
 }
+
+export {};
+
 let cached = global.mongoose;
-if(!cached){
-    cached = global.mongoose = {conn:null , promise:null};
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
-async function connectDB(){
-    if(cached.conn){
-        return cached.conn;
-    }
-    if(!cached.promise){
-        const options = {
-            bufferCommands:false,
-            maxPoolSize:10,
-            serviceSelectionTimeoutMS:5000,
-            socketTimeoutMS:45000,
-            family:4
-        };
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
 
-        cached.promise = mongoose.connect(mongodb_url , options)
-        .then((mongoose)=>{
-            console.log("mongodb connected successfully");
-            return mongoose
-        })
-    }
-    try {
+  if (!cached.promise) {
+    const options = {
+      bufferCommands: false,
+      maxPoolSize: 10,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+    };
+
+    cached.promise = mongoose.connect(mongodb_url, options).then((mongoose) => {
+      console.log("MongoDB connected successfully");
+      return mongoose;
+    });
+  }
+
+  try {
     cached.conn = await cached.promise;
-  } catch (e) {
+  } catch (error) {
     cached.promise = null;
-    console.error("MongoDB connection error:", e);
-    throw e;
+    console.error("MongoDB connection error:", error);
+    throw error;
   }
 
   return cached.conn;
